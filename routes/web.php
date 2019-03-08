e409d2ba381c60aeb659ad314df810802a43a008
cnagama2 <chikako_mkthrk@yahoo.co.jp>
2019-03-08 05:24:13 +0000

    listing MVC作成

--- routes/web.php
+++ routes/web.php
@@ -1,19 +1,28 @@
-<?php
+//===ここから削除（トップページをリスト一覧画面にするため。未ログイン時はログイン画面に遷移します）===
+//Route::get('/', function () {
+//   return view('welcome');
+//});
+//===ここまで削除===
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
+//===ここから追加===
+//リスト一覧画面
+Route::get('/','ListingsController@index');
 
-Route::get('/', function () {
-    return view('welcome');
-});
+//リスト新規画面
+Route::get('/new', 'ListingsController@new')->name('new');
+
+//リスト新規処理
+Route::post('/listings','ListingsController@store');
+
+//リスト更新画面
+Route::get('/listingsedit/{listing_id}', 'ListingsController@edit');
+
+//リスト更新処理
+Route::post('/listing/edit','ListingsController@update');
+
+//リスト削除処理
+Route::get('/listingsdelete/{listing_id}', 'ListingsController@destroy');
+//===ここまで追加===
 
 Auth::routes();
 
